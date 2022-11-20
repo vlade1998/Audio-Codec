6400049ab3539981034347b9252c3d2f87cb4ffe
vlademir <vladejrs@hotmail.com>
2022-11-20 20:02:21 -0300

    fix: overdrive

--- overdrive.v
+++ overdrive.v
@@ -6,22 +6,23 @@ module overdrive #(parameter DATA_WIDTH = 24) (
     output reg signed[(DATA_WIDTH-1):0] audio_left_out
 );
 
-    reg[DATA_WIDTH-1:0] threshold = 24'd8388600;
+    reg signed [DATA_WIDTH-1:0] threshold = 24'd100;
 
     always @ (negedge clk) begin
-        if (audio_right_in <= -threshold)
-            audio_right_out = -threshold;
-        else if (audio_right_in > -threshold && audio_right_in < threshold)
-            audio_right_out = audio_right_in;
+        if (audio_right_in > threshold) 
+          audio_right_out = threshold;
+        else if (audio_right_in < -threshold)
+          audio_right_out = -threshold;
         else
-            audio_right_out = threshold;
+          audio_right_out = audio_right_in;
 
-        if (audio_left_in <= -threshold)
-            audio_left_out = -threshold;
-        else if (audio_left_in > -threshold && audio_left_in < threshold)
-            audio_left_out = audio_left_in;
+        if (audio_left_in > threshold) 
+          audio_left_out = threshold;
+        else if (audio_left_in < -threshold)
+          audio_left_out = -threshold;
         else
-            audio_left_out = threshold;
+          audio_left_out = audio_left_in;
+
     end
 
 endmodule
